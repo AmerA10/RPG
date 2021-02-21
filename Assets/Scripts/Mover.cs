d7b66ee6b57072c132969ffab0c4b0780a692392
AmerA10 <ajami.amer1@gmail.com>
2021-02-21 16:33:08 -0700

    Initial commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Transform target;
+    NavMeshAgent nav;
+
+    Ray lastRay;
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        nav.destination = target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+           
+        }
+       
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit =  Physics.Raycast(ray, out hit); //takes the ray and hit and stores info into the hit, the method is a bool return type
+        Debug.DrawRay(ray.origin, ray.direction * 100, Color.red, 1f);
+        if (hasHit)
+        {
+            nav.destination = hit.point;
+            
+        }
+        
+    }
+}
