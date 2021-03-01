a808a3e82cd3cf6de4c635f425e11083a8d52589
AmerA10 <ajami.amer1@gmail.com>
2021-02-28 20:31:48 -0700

    Added Attack Animation, Prefab variants
    Created a base Character prefab
    from which the enemy and player prefabs are based upon with changes to accomodate each as well as added a health combonent and the ability for the player to attack

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -9,10 +9,16 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
+        [SerializeField] float weaponDamage = 5f;
         Transform target;
+
+        float timeSinceLastAttack = 0f;
 
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
+            
             if (target == null) return;
 
             if (!GetIsInRange())
@@ -23,7 +29,25 @@ namespace RPG.Combat
             else
             {
                 GetComponent<Mover>().Cancel(); //this cancel is used to stop moving for the range
+                if (timeSinceLastAttack >= timeBetweenAttacks)
+                {
+                    AttackBehaviour();
+                    timeSinceLastAttack = 0;
+                }
+            
             }
+        }
+
+        private void AttackBehaviour()
+        {
+            //This will trigger the Hit() Event 
+            GetComponent<Animator>().SetTrigger("attack"); //Trigger the attakc animation
+        }
+
+        void Hit()
+        {
+            Health healthComponent = target.GetComponent<Health>();
+            healthComponent.TakeDamage(weaponDamage);
         }
 
         private bool GetIsInRange()
@@ -42,6 +66,8 @@ namespace RPG.Combat
             target = null;
            
         }
+        //Animation Event
+
     }
 
 }
