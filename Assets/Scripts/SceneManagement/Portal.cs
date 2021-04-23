f477159baf39f5d8aed52065ecafa0a814d68cf5
AmerA10 <ajami.amer1@gmail.com>
2021-04-23 16:05:48 -0600

    added some scene management

---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement {
+
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] int sceneToLoad = -1;
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.transform.tag.Equals("Player"))
+            {
+                SceneManager.LoadScene(sceneToLoad);
+            }
+        }
+    }
+
+}
+
+
