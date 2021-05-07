0fc7608c42d538409a022489834c078fedb85be4
AmerA10 <ajami.amer1@gmail.com>
2021-05-06 18:56:09 -0600

    asd

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -2,18 +2,81 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using RPG.Control;
+using UnityEngine.AI;
 
 namespace RPG.SceneManagement {
 
     public class Portal : MonoBehaviour
     {
+        
+        enum DesitnationIdentified
+        {
+            A,B,C,D,E
+        }
+
         [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint;
+        [SerializeField] DesitnationIdentified destination;
         private void OnTriggerEnter(Collider other)
         {
             if(other.transform.tag.Equals("Player"))
             {
-                SceneManager.LoadScene(sceneToLoad);
+                StartCoroutine(Transition());
             }
+        }
+        //transition to new scene , load scene
+        private IEnumerator Transition()
+        {
+            if(sceneToLoad < 0)
+            {
+                Debug.LogError("Scene to load not set");
+                //yield break breaks the coroutine and stops it
+                yield break;
+            }
+            DontDestroyOnLoad(gameObject); //works only on root of scene, not child of another game object
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+            //get hold of the other portal in the loaded level
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
+            Debug.Log("Scene Loaded");
+            Destroy(gameObject); //destry gameobject after scene is loaded
+        }
+
+        private Portal GetOtherPortal()
+        {
+            //grabs the portal in the loaded scene that has the same enum destination as the current
+            Portal[] otherPortal = FindObjectsOfType<Portal>();
+            foreach(Portal portal in otherPortal) {
+                if (portal == this) continue; //just move on
+                if (portal.destination == this.destination) return portal;
+                else continue;
+
+
+
+
+
+
+            }
+            return null;
+        }
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            //makes the player position equal to the other portals spawn point position. Rememeber that this script is running in the previous scene
+            //as such the this.spawnPoint is the incorrect onoe
+            //sometimes the navmesh agent conflicts with teleportation that is happening
+            //   player.transform.position = otherPortal.spawnPoint.position; so the solution to this is to use the navmesh.warp method
+            //rotation does not conflict
+            //there will be another solution later on
+
+            GameObject player =  GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+           
+            
         }
     }
 
