ebf436467994a2a29671dc06b155b864a9f99906
AmerA10 <ajami.amer1@gmail.com>
2021-05-30 16:08:02 -0600

    Added a GUID to every entitiy
    Using the Unity SerializedObject
    added a GUID to every entitiy
    and used gameObject.scene.path
    if its empty its a prefab. SO now prefabs dont have any guid as they should

--- Assets/Scripts/Saving/SaveableEntity.cs
+++ Assets/Scripts/Saving/SaveableEntity.cs
@@ -2,13 +2,18 @@
 using UnityEditor;
 using System;
 
+
+
 namespace RPG.Saving
 {
+    [ExecuteAlways]
     public class SaveableEntity : MonoBehaviour
     {
+        //generates a GUID identifier and converst to String
+        [SerializeField] string uniqueIdentifier = "";
         public string GetUniqueIdentifier()
         {
-            return "";
+            return uniqueIdentifier;
         }
         public object CaptureState()
         {
@@ -21,6 +26,25 @@ namespace RPG.Saving
             Debug.Log("Restoring state for: " + GetUniqueIdentifier());
         }
 
+        private void Update()
+        {
+       
+            if (Application.IsPlaying(gameObject)) return ;
+            //prefabs do not have a path therefore it is possible to have no modifiers
+            if (string.IsNullOrEmpty(gameObject.scene.path)) return;
+
+
+            SerializedObject serializedObject = new SerializedObject(this);
+            //since the serialized property is genereic there is defined value type 
+            SerializedProperty property = serializedObject.FindProperty("uniqueIdentifier");
+            
+
+            if(string.IsNullOrEmpty(property.stringValue))
+            {
+                property.stringValue = System.Guid.NewGuid().ToString();
+                serializedObject.ApplyModifiedProperties();
+            }
+        }
 
 
     }
